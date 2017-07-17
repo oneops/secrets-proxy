d749d5ec83699ec48a9c448640a3642ac828fa9d
Suresh G <sureshg.g@gmail.com>
2017-07-17 10:52:07 -0700

    Authz & token configurations.

--- src/main/java/com/oneops/proxy/authz/JWTAuthFilter.java
+++ src/main/java/com/oneops/proxy/authz/JWTAuthFilter.java
@@ -15,7 +15,7 @@
  *   limitations under the License.
  *
  *******************************************************************************/
-package com.oneops.proxy.auth;
+package com.oneops.proxy.authz;
 
 import org.springframework.web.filter.GenericFilterBean;
 
--- src/main/java/com/oneops/proxy/authz/JWTAuthService.java
+++ src/main/java/com/oneops/proxy/authz/JWTAuthService.java
@@ -15,7 +15,7 @@
  *   limitations under the License.
  *
  *******************************************************************************/
-package com.oneops.proxy.auth;
+package com.oneops.proxy.authz;
 
 import com.oneops.proxy.config.OneOpsConfig;
 import io.jsonwebtoken.*;
