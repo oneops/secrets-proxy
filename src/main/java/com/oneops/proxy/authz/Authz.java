57c766d00af024f1e8fe203093391ca13db86ad6
Suresh G <sureshg.g@gmail.com>
2017-08-02 05:10:48 -0700

    Secret admin access change.

--- src/main/java/com/oneops/proxy/authz/Authz.java
+++ src/main/java/com/oneops/proxy/authz/Authz.java
@@ -62,12 +62,26 @@ public class Authz {
         }
 
         List<OneOpsTeam> teams = userRepo.getTeams(user.getUsername(), appGroup);
-        boolean hasAccess = teams.stream().anyMatch(t -> t.isSecretsAdmin(appGroup.getAssembly()));
+        boolean hasAccess = teams.stream().anyMatch(team -> hasAdminAccess(team, appGroup));
         if (!hasAccess) {
             throw new AuthorizationServiceException("OneOps user '" + user.getCn() + "' is not a '"
                     + SECRETS_ADMIN_TEAM + "' or not authorized to manage the secrets for environment: "
                     + appGroup.getNsPath());
         }
         return true;
+    }
+
+    /**
+     * Checks if the given team is a 'secret-admin' and has access to manage
+     * design and transition for the given application group.
+     *
+     * @param team     OneOps team
+     * @param appGroup Application group
+     * @return <code>true</code> if the team has admin access.
+     */
+    public boolean hasAdminAccess(OneOpsTeam team, AppGroup appGroup) {
+        return team.isSecretsAdmin(appGroup.getAssembly())
+                && team.isDesign()
+                && team.isTransition();
     }
 }
